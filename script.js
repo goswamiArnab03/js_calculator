c9711689274fc98285ee9202c7eee270a6c68cbf
goswamiArnab03 <arnabgoswami812@gmail.com>
2025-02-10 12:56:30 +0530

    script.js update

--- script.js
+++ script.js
@@ -18,3 +18,15 @@ function calculate(){
 function clearall(){
     display.value="";
 }
+
+
+function getRandomColor(){
+    let val1=Math.ceil(0 + Math.random()*255) ;
+    let val2=Math.ceil(0 + Math.random()*255) ;
+    let val3=Math.ceil(0 + Math.random()*255) ;
+    return `rgb(${val1},${val2},${val3})`;
+}
+document.body.style.transition = "background-color 1s ease-in-out";
+let a = setInterval(()=>{
+    document.body.style.background=getRandomColor();
+},1000);
